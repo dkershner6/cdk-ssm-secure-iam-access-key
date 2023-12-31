b85b9b4f2e17050d0138ed5b81f899acc1763f21
Derek Kershner <dkershner@dkershner.com>
2023-12-31 13:59:55 -0800

    fix: More languages, construct lib

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,20 @@
-import { assertions, App, Stack } from 'aws-cdk-lib';
-import { ISSMSecureIAMAccessKeyProps, SSMSecureIAMAccessKey } from '../src';
+import { assertions, App, Stack } from "aws-cdk-lib";
+import { ISSMSecureIAMAccessKeyProps, SSMSecureIAMAccessKey } from "../src";
 
 const mockApp = new App();
-const stack = new Stack(mockApp, 'TestStack');
+const stack = new Stack(mockApp, "TestStack");
 
-it('Should deploy', () => {
-  const testProps: ISSMSecureIAMAccessKeyProps = {
-    parameterName: '/test/param',
-    // @ts-expect-error - Incomplete Mock
-    user: {
-      userName: 'testUserName',
-      userArn: 'testUserArn',
+it("Should deploy", () => {
+    const testProps: ISSMSecureIAMAccessKeyProps = {
+        parameterName: "/test/param",
+        // @ts-expect-error - Incomplete Mock
+        user: {
+            userName: "testUserName",
+            userArn: "testUserArn",
+        },
+    };
 
-    },
-  };
+    new SSMSecureIAMAccessKey(stack, "test", testProps);
 
-  new SSMSecureIAMAccessKey(stack, 'test', testProps);
-
-  expect(assertions.Template.fromStack(stack).toJSON()).toMatchSnapshot();
+    expect(assertions.Template.fromStack(stack).toJSON()).toMatchSnapshot();
 });
